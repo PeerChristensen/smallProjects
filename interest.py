4ae1c2bf0faaf9da6507e147a86db661f896de14
Peer Christensen <hr.pchristensen@gmail.com>
2017-05-04 11:35:33 +0200

    Update interest.py

--- interest.py
+++ interest.py
@@ -6,20 +6,12 @@ def interest(amount, interest, years,deposit,tax):
     calculates the balance of a
     savings account based on the initial amount,
     the yearly interest in %, number of years, monthly deposits and tax
+    
+    example:
     >>> interest(250000, 1, 1)
     252500
     '''
-    '''
-    non_tax = amount
-    tax_amount = 0
-    for year in range(years):
-        non_tax += deposit*12
-        tax_amount += non_tax * (float(interest)/float(100))
-        print(tax_amount)
-        amount = non_tax + (tax_amount * (100-tax)/100)
     
-    return amount
-    '''
     tax_amount=0
     new_amount=0
     for year in range(years):
